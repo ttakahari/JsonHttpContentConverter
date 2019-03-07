7d7d1566d86da50f1855583b67184af2a621ebc1
t.takahari <taiga1018@gmail.com>
2019-03-07 14:44:18 +0900

    add Jil implementation.

---
+++ src/JsonHttpContentConverter.Jil/JilHttpContentConverter.cs
@@ -0,0 +1,46 @@
+﻿using Jil;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace JsonHttpContentConverter.Jil
+{
+    /// <summary>
+    /// Convert between an object and <see cref="HttpContent"/> that includes JSON with using Jil.
+    /// </summary>
+    public class JilHttpContentConverter : IJsonHttpContentConverter
+    {
+        private readonly Options _options;
+
+        /// <summary>
+        /// Create a new instance of <see cref="JilHttpContentConverter"/>.
+        /// </summary>
+        public JilHttpContentConverter()
+            : this(Options.Default)
+        {
+        }
+
+        /// <summary>
+        /// Create a new instance of <see cref="JilHttpContentConverter"/> with an instance of <see cref="Options"/>.
+        /// </summary>
+        /// <param name="options">An instance of <see cref="Options"/>.</param>
+        public JilHttpContentConverter(Options options)
+            => _options = options ?? throw new ArgumentNullException(nameof(options));
+
+        /// <inheritdoc />
+        public HttpContent ToJsonHttpContent<T>(T value)
+        {
+            var json = JSON.Serialize(value, _options);
+
+            return new StringContent(json);
+        }
+
+        /// <inheritdoc />
+        public async Task<T> FromJsonHttpContent<T>(HttpContent content)
+        {
+            var json = await content.ReadAsStringAsync().ConfigureAwait(false);
+
+            return JSON.Deserialize<T>(json, _options);
+        }
+    }
+}
