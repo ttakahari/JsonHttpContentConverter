afdc6a81082f08a05486f2187c15309f3ebe7057
t.takahari <taiga1018@gmail.com>
2019-03-07 18:26:11 +0900

    add DI implementation.

---
+++ src/JsonHttpContentConverter.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,46 @@
+﻿using JsonHttpContentConverter;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Extensions of <see cref="IServiceCollection"/>.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Add <see cref="IJsonHttpContentConverter"/> to resolve an instance with DI.
+        /// </summary>
+        /// <typeparam name="TConverter">A type of <see cref="IJsonHttpContentConverter"/>.</typeparam>
+        /// <param name="services"><see cref="IServiceCollection"/> to add servcies to.</param>
+        /// <returns>Added <see cref="IServiceCollection"/>.</returns>
+        public static IServiceCollection AddJsonHttpContentConverter<TConverter>(this IServiceCollection services)
+            where TConverter : IJsonHttpContentConverter
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            services.AddSingleton(typeof(IJsonHttpContentConverter), typeof(TConverter));
+
+            return services;
+        }
+
+        /// <summary>
+        /// Add <see cref="IJsonHttpContentConverter"/> to resolve an instance with DI.
+        /// </summary>
+        /// <param name="services"><see cref="IServiceCollection"/> to add servcies to.</param>
+        /// <param name="converter">An instance of <see cref="IJsonHttpContentConverter"/>.</param>
+        /// <returns>Added <see cref="IServiceCollection"/>.</returns>
+        public static IServiceCollection AddJsonHttpContentConverter(this IServiceCollection services, IJsonHttpContentConverter converter)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+            if (converter == null) throw new ArgumentNullException(nameof(converter));
+
+            services.AddSingleton(converter);
+
+            return services;
+        }
+    }
+}
---
+++ test/JsonHttpContentConverter.DependencyInjection.Tests/ServiceCollectionTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace JsonHttpContentConverter.DependencyInjection.Tests
+{
+    public class ServiceCollectionTests
+    {
+        [Fact]
+        public void AddJsonHttpContentConverter_Tests()
+        {
+            // by type.
+            {
+                var services = new ServiceCollection();
+
+                services.AddJsonHttpContentConverter<NullJsonHttpContentConverter>();
+
+                var service = services.BuildServiceProvider();
+
+                var converter = service.GetService<IJsonHttpContentConverter>();
+
+                Assert.NotNull(converter);
+                Assert.IsType<NullJsonHttpContentConverter>(converter);
+            }
+
+            // by instance.
+            {
+                var services = new ServiceCollection();
+
+                Assert.Throws<ArgumentNullException>(() => services.AddJsonHttpContentConverter(null));
+
+                services.AddJsonHttpContentConverter(new NullJsonHttpContentConverter());
+
+                var service = services.BuildServiceProvider();
+
+                var converter = service.GetService<IJsonHttpContentConverter>();
+
+                Assert.NotNull(converter);
+                Assert.IsType<NullJsonHttpContentConverter>(converter);
+            }
+        }
+    }
+
+    public class NullJsonHttpContentConverter : IJsonHttpContentConverter
+    {
+        public HttpContent ToJsonHttpContent<T>(T value)
+            => throw new NotImplementedException();
+
+        public Task<T> FromJsonHttpContent<T>(HttpContent content)
+            => throw new NotImplementedException();
+    }
+}
